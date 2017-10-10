8b9af07283ae2494bd04bb1845e93ca27cb846a5
daydreamboy <wesley4chen@gmail.com>
2017-10-10 16:54:28 +0800

    add HelloXcodebuild

---
+++ HelloXcodebuild/HelloXcodebuildForApp/HelloXcodebuildForApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloXcodebuildForApp
+//
+//  Created by wesley_chen on 10/10/2017.
+//  Copyright © 2017 wesley_chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HelloXcodebuild/HelloXcodebuildForCocoaTouchFramework/HelloXcodebuildForCocoaTouchFramework/HelloXcodebuildForCocoaTouchFramework.h
@@ -0,0 +1,19 @@
+//
+//  HelloXcodebuildForCocoaTouchFramework.h
+//  HelloXcodebuildForCocoaTouchFramework
+//
+//  Created by wesley_chen on 10/10/2017.
+//  Copyright © 2017 wesley_chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HelloXcodebuildForCocoaTouchFramework.
+FOUNDATION_EXPORT double HelloXcodebuildForCocoaTouchFrameworkVersionNumber;
+
+//! Project version string for HelloXcodebuildForCocoaTouchFramework.
+FOUNDATION_EXPORT const unsigned char HelloXcodebuildForCocoaTouchFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HelloXcodebuildForCocoaTouchFramework/PublicHeader.h>
+
+
