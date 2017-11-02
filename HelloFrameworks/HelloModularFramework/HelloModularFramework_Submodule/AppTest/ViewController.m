ddda601163ec38a9a852066d1d8fadac3bde201d
daydreamboy <wesley4chen@gmail.com>
2017-11-02 11:33:26 +0800

    HelloProjects: add HelloModularFramework_Submodule

---
+++ HelloFrameworks/HelloModularFramework/HelloModularFramework_Submodule/AppTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AppTest
+//
+//  Created by wesley_chen on 25/10/2017.
+//  Copyright © 2017 wesley_chen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+//#import <HelloModularFramework_Submodule/HelloModularFramework_Submodule.h>
+@import HelloModularFramework_Submodule;
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    [PublicClassA doSomething];
+    [PublicClassB doSomething];
+}
+
+@end
