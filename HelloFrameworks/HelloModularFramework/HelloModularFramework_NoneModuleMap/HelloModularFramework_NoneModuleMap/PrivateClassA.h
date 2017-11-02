0fd3709152ce5a17adcd9278f31b3e6d472856fd
daydreamboy <wesley4chen@gmail.com>
2017-11-02 20:43:01 +0800

    adjust HelloModularFramework

---
+++ HelloFrameworks/HelloModularFramework/HelloModularFramework_NoneModuleMap/HelloModularFramework_NoneModuleMap/PrivateClassA.h
@@ -0,0 +1,13 @@
+//
+//  PrivateClassA.h
+//  HelloModularFramework_Default
+//
+//  Created by wesley_chen on 25/10/2017.
+//  Copyright © 2017 wesley_chen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PrivateClassA : NSObject
++ (void)doSomething;
+@end
