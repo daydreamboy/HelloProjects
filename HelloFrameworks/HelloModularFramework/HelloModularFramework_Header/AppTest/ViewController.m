c35ad28c38616e941b05d5bf244d16b3ac11809b
daydreamboy <wesley4chen@gmail.com>
2017-11-02 20:51:12 +0800

    adjust HelloModularFramework

---
+++ HelloFrameworks/HelloModularFramework/HelloModularFramework_Header/AppTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AppTest
+//
+//  Created by wesley_chen on 25/10/2017.
+//  Copyright © 2017 wesley_chen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+//#import <HelloModularFramework_Header/HelloModularFramework_Header.h>
+@import HelloModularFramework_Header;
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    [PublicClassA doSomething];
+    [PublicClassB doSomething];
+}
+
+@end
