4d2eca7c1a7c00a125a26203f1453cfc53b1802e
daydreamboy <wesley4chen@gmail.com>
2017-11-16 10:28:56 +0800

    HelloFrameworks: add HelloStaticLibrary

---
+++ HelloFrameworks/HelloStaticLibrary/HelloStaticLibrary/HelloStaticLibrary.h
@@ -0,0 +1,13 @@
+//
+//  HelloStaticLibrary.h
+//  HelloStaticLibrary
+//
+//  Created by wesley_chen on 16/11/2017.
+//  Copyright © 2017 wesley_chen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HelloStaticLibrary : NSObject
+
+@end
---
+++ HelloFrameworks/HelloStaticLibrary/HelloStaticLibrary/HelloStaticLibrary.m
@@ -0,0 +1,13 @@
+//
+//  HelloStaticLibrary.m
+//  HelloStaticLibrary
+//
+//  Created by wesley_chen on 16/11/2017.
+//  Copyright © 2017 wesley_chen. All rights reserved.
+//
+
+#import "HelloStaticLibrary.h"
+
+@implementation HelloStaticLibrary
+
+@end
