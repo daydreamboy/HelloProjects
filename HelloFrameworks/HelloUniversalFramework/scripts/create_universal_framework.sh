eb0583a2ab30df4f7690cb274462c1b7687d0690
daydreamboy <wesley4chen@gmail.com>
2017-12-08 17:59:05 +0800

    update create_universal_framework.sh

--- HelloFrameworks/HelloUniversalFramework/scripts/create_universal_framework.sh
+++ HelloFrameworks/HelloUniversalFramework/scripts/create_universal_framework.sh
@@ -18,26 +18,37 @@ if [ "Debug" == ${CONFIGURATION} ]; then
 
         export ALREADYINVOKED="true"
 
+        additional_arch=${PLATFORM_NAME}
         if [ ${PLATFORM_NAME} = "iphonesimulator" ]; then
-            xcodebuild -target "${PROJECT_NAME}" ONLY_ACTIVE_ARCH=NO -configuration ${CONFIGURATION} -sdk iphoneos BUILD_DIR="${BUILD_DIR}" BUILD_ROOT="${BUILD_ROOT}" clean build
+            additional_arch='iphoneos'
         else
-            xcodebuild -target "${PROJECT_NAME}" -configuration ${CONFIGURATION} -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO BUILD_DIR="${BUILD_DIR}" BUILD_ROOT="${BUILD_ROOT}" clean build
+            additional_arch='iphonesimulator'
         fi
 
+        # if [ ${PLATFORM_NAME} = "iphonesimulator" ]; then
+        #     xcodebuild -target "${PROJECT_NAME}" ONLY_ACTIVE_ARCH=NO -configuration ${CONFIGURATION} -sdk iphoneos BUILD_DIR="${BUILD_DIR}" BUILD_ROOT="${BUILD_ROOT}" clean build
+        # else
+        #     xcodebuild -target "${PROJECT_NAME}" -configuration ${CONFIGURATION} -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO BUILD_DIR="${BUILD_DIR}" BUILD_ROOT="${BUILD_ROOT}" clean build
+        # fi
+        
+        xcodebuild -target "${PRODUCT_NAME}" -configuration ${CONFIGURATION} -sdk ${additional_arch} ONLY_ACTIVE_ARCH=NO BUILD_DIR="${BUILD_DIR}" BUILD_ROOT="${BUILD_ROOT}" OBJROOT="${OBJROOT}" clean build
+        
         # Step 2. Copy the framework structure (from iphoneos build) to the universal folder
-        cp -R "${BUILD_DIR}/${CONFIGURATION}-iphoneos/${PRODUCT_NAME}.framework" "${UNIVERSAL_OUTPUTFOLDER}/"
+        rsync -arv "${BUILD_DIR}/${CONFIGURATION}-${additional_arch}/${PRODUCT_NAME}.framework" "${UNIVERSAL_OUTPUTFOLDER}/"
+
+        # cp -R "${BUILD_DIR}/${CONFIGURATION}-iphoneos/${PRODUCT_NAME}.framework" "${UNIVERSAL_OUTPUTFOLDER}/"
 
         # Step 3. Copy Swift modules from iphonesimulator build (if it exists) to the copied framework directory
         SIMULATOR_SWIFT_MODULES_DIR="${BUILD_DIR}/${CONFIGURATION}-iphonesimulator/${PRODUCT_NAME}.framework/Modules/${PROJECT_NAME}.swiftmodule/."
         if [ -d "${SIMULATOR_SWIFT_MODULES_DIR}" ]; then
-            cp -R "${SIMULATOR_SWIFT_MODULES_DIR}" "${UNIVERSAL_OUTPUTFOLDER}/${PRODUCT_NAME}.framework/Modules/${PROJECT_NAME}.swiftmodule"
+            rsync -arv "${SIMULATOR_SWIFT_MODULES_DIR}" "${UNIVERSAL_OUTPUTFOLDER}/${PRODUCT_NAME}.framework/Modules/${PRODUCT_NAME}.swiftmodule"
         fi
 
         # Step 4. Create universal binary file using lipo and place the combined executable in the copied framework directory
         lipo -create -output "${UNIVERSAL_OUTPUTFOLDER}/${PRODUCT_NAME}.framework/${PRODUCT_NAME}" "${BUILD_DIR}/${CONFIGURATION}-iphonesimulator/${PRODUCT_NAME}.framework/${PRODUCT_NAME}" "${BUILD_DIR}/${CONFIGURATION}-iphoneos/${PRODUCT_NAME}.framework/${PRODUCT_NAME}"
 
         # Step 5. Convenience step to copy the framework to the project's directory
-        cp -R "${UNIVERSAL_OUTPUTFOLDER}/${PRODUCT_NAME}.framework" "${PROJECT_DIR}"
+        rsync -arv "${UNIVERSAL_OUTPUTFOLDER}/${PRODUCT_NAME}.framework" "${PROJECT_DIR}"
 
         # Step 6. Convenience step to open the project's directory in Finder
         open "${PROJECT_DIR}"
