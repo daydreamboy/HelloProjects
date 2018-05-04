9911d8f172dd60f3eea13b247a604f139bff1894
daydreamboy <wesley4chen@gmail.com>
2018-05-04 10:28:32 +0800

    HelloNSRegularExpression: add code

--- HelloNSRegularExpression/Tests/Tests.m
+++ HelloNSRegularExpression/Tests/Tests.m
@@ -64,9 +64,10 @@
     [self runRegexWithPattern:pattern matchString:matchString];
 }
 
-- (void)runRegexWithPattern:(NSString *)pattern matchString:(NSString *)matchString {
+- (NSArray<NSValue *> *)runRegexWithPattern:(NSString *)pattern matchString:(NSString *)matchString {
     NSLog(@"--------------------------------");
     NSError *error;
+    NSMutableArray *ranges = [NSMutableArray array];
     NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:pattern options:kNilOptions error:&error];
     [regex enumerateMatchesInString:matchString options:kNilOptions range:NSMakeRange(0, matchString.length) usingBlock:^(NSTextCheckingResult * _Nullable result, NSMatchingFlags flags, BOOL * _Nonnull stop) {
         
@@ -79,11 +80,15 @@
             NSRange captureRange = [result rangeAtIndex:index];
             if (captureRange.location != NSNotFound) {
                 NSLog(@"captureRange: %@ - %@", NSStringFromRange(captureRange), [matchString substringWithRange:captureRange]);
+                
+                [ranges addObject:[NSValue valueWithRange:captureRange]];
             }
         }
     }];
     
     NSLog(@"--------------------------------");
+    
+    return ranges;
 }
 
 - (void)test_domain {
@@ -149,6 +154,176 @@
     pattern = @"^\\{\\{(.+),(.+)\\},\\{(.+),(.+)\\}\\}$";
     matchString = @"{{1,2},{3,4}}";
     [self runRegexWithPattern:pattern matchString:matchString];
+}
+
+#define STR_OF_JSON(...) @#__VA_ARGS__
+
+- (void)test_velocity_variable {
+    NSString *pattern;
+    NSString *matchString;
+    
+    pattern = @"(\\$!?(?:[a-zA-Z]+[a-zA-Z0-9_]*|\\{[a-zA-Z]+[a-zA-Z0-9_]*\\}))";
+    matchString = STR_OF_JSON(
+    {
+        "body": {
+            "match1": "¥$amount",
+            "match2": "¥$!amount2",
+            "match3": "满$!{startFee}.5元使用",
+            "match4": "满$!{s1}元使用",
+            "match5": "满${s1}元使用",
+            "match6": "满$s_s.5元使用",
+            "not_match1": "满$!{startFee.5元使用",
+            "not_match2": "满$5元使用",
+            "not_match3": "满$_5元使用",
+        }
+    }
+    );
+    [self runRegexWithPattern:pattern matchString:matchString];
+}
+
+// Note: 替换变量采用拆分components然后组合components方式
+- (void)test_velocity_variable_interpolation_by_substring_components {
+    NSString *pattern;
+    NSString *matchString;
+    
+    NSDictionary *config = @{
+                                    @"amount": @"1",
+                                    @"amount2": @"2",
+                                    @"startFee": @"3",
+                                    };
+    
+    pattern = @"(\\$!?(?:[a-zA-Z]+[a-zA-Z0-9_]*|\\{[a-zA-Z]+[a-zA-Z0-9_]*\\}))";
+    matchString = STR_OF_JSON(
+                              {
+                                  "body": {
+                                      "match1": "¥$amount",
+                                      "match2": "¥$!amount2",
+                                      "match3": "满$!{startFee}.5元使用",
+                                      "match4": "满$!{s1}元使用",
+                                      "match5": "满${s1}元使用",
+                                      "match6": "满$s_s.5元使用",
+                                      "match7": "满${startFee}.5元使用",
+                                      "not_match1": "满$!{startFee.5元使用",
+                                      "not_match2": "满$5元使用",
+                                      "not_match3": "满$_5元使用",
+                                  }
+                              }
+                              );
+    
+    NSMutableArray *substringParts = [NSMutableArray array];
+    NSArray *ranges = [self runRegexWithPattern:pattern matchString:matchString];
+    
+    NSRange previousRange = NSMakeRange(0, 0);
+    for (NSInteger i = 0; i < ranges.count; i++) {
+        NSValue *value = ranges[i];
+        // Note: suppose range is always valid
+        NSRange range = [value rangeValue];
+        NSRange gap = NSMakeRange(previousRange.location + previousRange.length, range.location - previousRange.location - previousRange.length);
+        
+        [substringParts addObject:[matchString substringWithRange:gap]];
+        
+        NSString *originString = [matchString substringWithRange:range];
+        NSString *variableName = [originString stringByTrimmingCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"$!{}"]];
+        BOOL useDefaultEmptyString = [originString hasPrefix:@"$!"];
+        NSString *substitueString = config[variableName] ? config[variableName] : (useDefaultEmptyString ? @"" : nil);
+        if (substitueString) {
+            [substringParts addObject:substitueString];
+        }
+        else {
+            [substringParts addObject:originString];
+        }
+        if (i == ranges.count - 1) {
+            NSRange lastRange = NSMakeRange(range.location + range.length, matchString.length - range.location - range.length);
+            NSString *lastPartSubstring = [matchString substringWithRange:lastRange];
+            [substringParts addObject:lastPartSubstring];
+        }
+        
+        previousRange = range;
+    }
+    
+    NSString *translatedString = [substringParts componentsJoinedByString:@""];
+    
+    NSLog(@"%@", substringParts);
+    NSLog(@"%@", translatedString);
+    
+    id jsonObject = [NSJSONSerialization JSONObjectWithData:[translatedString dataUsingEncoding:NSUTF8StringEncoding] options:NSJSONReadingMutableLeaves error:nil];
+    NSLog(@"jsonObject: %@", jsonObject);
+}
+
+- (void)test_velocity_variable_interpolation_by_traverse_match_first {
+    NSString *pattern;
+    NSString *matchString;
+    
+    NSDictionary *config = @{
+                             @"amount": @"1",
+                             @"amount2": @"2",
+                             @"startFee": @"3",
+                             };
+    
+    pattern = @"(\\$!?(?:[a-zA-Z]+[a-zA-Z0-9_]*|\\{[a-zA-Z]+[a-zA-Z0-9_]*\\}))";
+    matchString = STR_OF_JSON(
+                              {
+                                  "body": {
+                                      "match1": "¥$amount",
+                                      "match2": "¥$!amount2",
+                                      "match3": "满$!{startFee}.5元使用",
+                                      "match4": "满$!{s1}元使用",
+                                      "match5": "满${s1}元使用",
+                                      "match6": "满$s_s.5元使用",
+                                      "match7": "满${startFee}.5元使用",
+                                      "not_match1": "满$!{startFee.5元使用",
+                                      "not_match2": "满$5元使用",
+                                      "not_match3": "满$_5元使用",
+                                  }
+                              }
+                              );
+    
+    matchString = STR_OF_JSON(
+                              {
+                                  "body": {
+                                      "not_match2": "满$5元使用",
+                                      "not_match3": "满$_5元使用",
+                                  }
+                              }
+                              );
+    
+    NSError *error;
+    NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:pattern options:kNilOptions error:&error];
+    
+    NSMutableString *translatedString = [matchString mutableCopy];
+    
+    BOOL matched;
+    NSRange range = NSMakeRange(0, translatedString.length);;
+    do {
+        if (!(range.location < translatedString.length && range.location + range.length <= translatedString.length)) {
+            break;
+        }
+        
+        NSTextCheckingResult *result = [regex firstMatchInString:translatedString options:kNilOptions range:range];
+        NSRange capturedRange = [result rangeAtIndex:1];
+        if (result && (capturedRange.location != NSNotFound && capturedRange.length)) {
+            matched = YES;
+            
+            NSString *originString = [translatedString substringWithRange:capturedRange];
+            NSString *variableName = [originString stringByTrimmingCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"$!{}"]];
+            BOOL useDefaultEmptyString = [originString hasPrefix:@"$!"];
+            NSString *substitueString = config[variableName] ? config[variableName] : (useDefaultEmptyString ? @"" : nil);
+            if (substitueString) {
+                [translatedString replaceCharactersInRange:capturedRange withString:substitueString];
+                range = NSMakeRange(capturedRange.location + substitueString.length, translatedString.length - capturedRange.location - substitueString.length);
+            }
+            else {
+                // Note: config没有替换的值而且不使用$!，则不替换
+                range = NSMakeRange(capturedRange.location + capturedRange.length, translatedString.length - capturedRange.location - capturedRange.length);
+            }
+        }
+        else {
+            matched = NO;
+        }
+    } while (matched);
+    NSLog(@"%@", translatedString);
+    id jsonObject = [NSJSONSerialization JSONObjectWithData:[translatedString dataUsingEncoding:NSUTF8StringEncoding] options:NSJSONReadingMutableLeaves error:nil];
+    NSLog(@"jsonObject: %@", jsonObject);
 }
 
 @end
