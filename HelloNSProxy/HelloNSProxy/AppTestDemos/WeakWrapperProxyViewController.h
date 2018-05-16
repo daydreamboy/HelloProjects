59eb818dc39b93e42af9773a8e9ee7086cc6dee4
daydreamboy <wesley4chen@gmail.com>
2018-05-16 16:51:30 +0800

    add HelloNSProxy

---
+++ HelloNSProxy/HelloNSProxy/AppTestDemos/WeakWrapperProxyViewController.h
@@ -0,0 +1,13 @@
+//
+//  WeakWrapperProxyViewController.h
+//  HelloNSProxy
+//
+//  Created by wesley_chen on 2018/5/16.
+//  Copyright © 2018 wesley_chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WeakWrapperProxyViewController : UIViewController
+
+@end
