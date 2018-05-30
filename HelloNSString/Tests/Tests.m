15f9535c813e6ee5c1c79f7a847f373420298c5e
daydreamboy <wesley4chen@gmail.com>
2018-05-30 22:35:54 +0800

    add HelloNSString

---
+++ HelloNSString/Tests/Tests.m
@@ -0,0 +1,113 @@
+//
+//  Tests.m
+//  Tests
+//
+//  Created by wesley_chen on 2018/5/30.
+//  Copyright © 2018 wesley_chen. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "WCStringTool.h"
+
+@interface Tests : XCTestCase
+
+@end
+
+@implementation Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+
+- (void)test_CGRectNull {
+    NSValue *value = [NSValue valueWithCGRect:CGRectNull];
+    NSLog(@"%@", value);
+    
+    CGRect rect = [value CGRectValue];
+    if (CGRectIsNull(rect)) {
+        NSLog(@"null rect");
+    }
+    else {
+        NSLog(@"rect: %@", NSStringFromCGRect(rect));
+    }
+}
+
+- (void)test_CGRectFromString {
+    NSString *str;
+    CGRect rect;
+    
+    str = @"{{1,2},{3,4}";
+    rect = CGRectFromString(str);
+    NSLog(@"rect: %@", NSStringFromCGRect(rect));
+    
+    str = @"{{1,2},{3,4}}";
+    rect = CGRectFromString(str);
+    NSLog(@"rect: %@", NSStringFromCGRect(rect));
+    
+    str = @"{{5,  6}, {7, 8}}";
+    rect = CGRectFromString(str);
+    NSLog(@"rect: %@", NSStringFromCGRect(rect));
+}
+
+- (void)test_rectFromString {
+    NSString *str;
+    
+    // Case 1
+    str = @"{{0,0 } , {0,0}}";
+    XCTAssertTrue(CGRectEqualToRect([WCStringTool rectFromString:str], CGRectZero));
+    
+    // Case 2
+    str = @"{{1,2},{3,4}}";
+    XCTAssertTrue(CGRectEqualToRect([WCStringTool rectFromString:str], CGRectFromString(str)));
+    
+    // Case 3
+    str = @"{{5,  6}   , {7, 8}}";
+    XCTAssertTrue(CGRectEqualToRect([WCStringTool rectFromString:str], CGRectFromString(str)));
+    
+    // Abnormal Case 1
+    str = @"{{1,2},{3,4}";
+    XCTAssertTrue(CGRectEqualToRect([WCStringTool rectFromString:str], CGRectNull));
+    XCTAssertTrue(CGRectIsNull([WCStringTool rectFromString:str]));
+    
+    // Abnormal Case 2: not allow 0.0, instead of just using 0.
+    str = @"{{0.0,0.0},{0.0,0.0}";
+    XCTAssertFalse(CGRectEqualToRect([WCStringTool rectFromString:str], CGRectZero));
+    XCTAssertTrue(CGRectIsNull([WCStringTool rectFromString:str]));
+}
+
+- (void)test_UIEdgeInsetsFromString {
+    NSString *str;
+    
+    // Case 1
+    str = @"{1,2,3,4}";
+    XCTAssertTrue(UIEdgeInsetsEqualToEdgeInsets([WCStringTool edgeInsetsValueFromString:str].UIEdgeInsetsValue, UIEdgeInsetsMake(1, 2, 3, 4)));
+    
+    // Case 2
+    str = @"{0,0,0,0}";
+    XCTAssertTrue(UIEdgeInsetsEqualToEdgeInsets([WCStringTool edgeInsetsValueFromString:str].UIEdgeInsetsValue, UIEdgeInsetsZero));
+    
+    // Case 3
+    str = @"{1.1,2.2,3.3,4.4}";
+    XCTAssertTrue(UIEdgeInsetsEqualToEdgeInsets([WCStringTool edgeInsetsValueFromString:str].UIEdgeInsetsValue, UIEdgeInsetsMake(1.1, 2.2, 3.3, 4.4)));
+    
+    // Case 3
+    str = @"{1.1,2.2,3.3,4.4}";
+    XCTAssertTrue(UIEdgeInsetsEqualToEdgeInsets([WCStringTool edgeInsetsValueFromString:str].UIEdgeInsetsValue, UIEdgeInsetsFromString(str)));
+    
+    // Abnornam Case 1
+    str = @"{0.0,0,0,0}";
+    XCTAssertNil([WCStringTool edgeInsetsValueFromString:str]);
+    
+    // Abnornam Case 2
+    str = @"{{1,2},{3,4}}";
+    XCTAssertNil([WCStringTool edgeInsetsValueFromString:str]);
+}
+
+@end
---
+++ HelloNSString/WCStringTool/WCStringTool.h
@@ -0,0 +1,42 @@
+//
+//  WCStringTool.h
+//  HelloNSString
+//
+//  Created by wesley_chen on 2018/5/30.
+//  Copyright © 2018 wesley_chen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface WCStringTool : NSObject
+
+#pragma mark - NSString to Struct/Object
+
+/**
+ Safe convert NSString to CGRect
+ 
+ @param string the NSString represents CGRect
+ @return the CGRect. If the NSString is malformed, return the CGRectNull
+ @warning not allow 0.0, instead of just using 0.
+ */
++ (CGRect)rectFromString:(NSString *)string;
+
+/**
+ Safe convert NSString to UIEdgeInsets
+
+ @param string the NSString represents UIEdgeInsets
+ @return the NSValue to wrap UIEdgeInsets, use value.UIEdgeInsets to get UIEdgeInsets
+ @warning not allow 0.0, instead of just using 0.
+ */
++ (NSValue *)edgeInsetsValueFromString:(NSString *)string;
+
+/**
+ Convert hex string to UIColor
+ 
+ @param string the hex string with foramt @"#RRGGBB" or @"#RRGGBBAA"
+ @return the UIColor object. return nil if string is not valid.
+ */
++ (UIColor *)colorFromHexString:(NSString *)string;
+
+@end
