61f14f1a005db1c2de85bcfd1d72e7e116da37ae
daydreamboy <wesley4chen@gmail.com>
2018-07-06 22:44:12 +0800

    add HelloUILabel

---
+++ HelloUILabel/HelloUILabel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloUILabel
+//
+//  Created by wesley_chen on 2018/6/5.
+//  Copyright © 2018 wesley_chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
