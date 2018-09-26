dd371847566ca70812c2728b4baa95cf3b19e1d9
daydreamboy <wesley4chen@gmail.com>
2018-09-26 11:43:50 +0800

    HelloUITextView: add code

--- HelloUITextView/HelloUITextView/AppTestDemos/Demo1ViewController.m
+++ HelloUITextView/HelloUITextView/AppTestDemos/Demo1ViewController.m
@@ -9,19 +9,50 @@
 #import "Demo1ViewController.h"
 
 @interface Demo1ViewController ()
-
+@property (nonatomic, strong) UITextView *textView;
 @end
 
 @implementation Demo1ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    UIBarButtonItem *rightBarItem1 = [[UIBarButtonItem alloc] initWithTitle:@"Insert" style:UIBarButtonItemStylePlain target:self action:@selector(rightBarItem1Clicked:)];
+    
+    UIBarButtonItem *rightBarItem2 = [[UIBarButtonItem alloc] initWithTitle:@"Dismiss" style:UIBarButtonItemStylePlain target:self action:@selector(rightBarItem2Clicked:)];
+    
+    self.navigationItem.rightBarButtonItems = @[rightBarItem1, rightBarItem2];
+    
+    [self.view addSubview:self.textView];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - Getters
+
+- (UITextView *)textView {
+    if (!_textView) {
+        CGSize screenSize = [[UIScreen mainScreen] bounds].size;
+        UITextView *textView = [[UITextView alloc] initWithFrame:CGRectMake(0, 60, screenSize.width, screenSize.height - 60)];
+        textView.text = @"Some Text";
+        
+        _textView = textView;
+    }
+    
+    return _textView;
+}
+
+#pragma mark - Actions
+
+- (void)rightBarItem1Clicked:(id)sender {
+    NSString *insertingString = @"InsertedText";
+    [self.textView replaceRange:self.textView.selectedTextRange withText:insertingString];
+}
+
+- (void)rightBarItem2Clicked:(id)sender {
+    NSLog(@"dismiss");
+    [self.textView resignFirstResponder];
+//    NSString *insertingString = @"InsertedText";
+//    [self.textView replaceRange:self.textView.selectedTextRange withText:insertingString];
 }
 
 @end
