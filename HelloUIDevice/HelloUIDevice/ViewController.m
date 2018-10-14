431348435bf4983063dd5e2a227d7612b5fffa8b
daydreamboy <wesley4chen@gmail.com>
2018-10-14 13:49:29 +0800

    add HelloUIDevice

---
+++ HelloUIDevice/HelloUIDevice/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  HelloUIDevice
+//
+//  Created by wesley_chen on 2018/10/10.
+//  Copyright © 2018 wesley_chen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
