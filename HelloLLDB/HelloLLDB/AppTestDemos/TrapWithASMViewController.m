424e446ac2ea69a66fa99d3283e79bf108fd9c20
daydreamboy <wesley4chen@gmail.com>
2019-01-24 15:14:55 +0800

    HelloLLDB: add code

--- HelloLLDB/HelloLLDB/AppTestDemos/TrapWithASMViewController.m
+++ HelloLLDB/HelloLLDB/AppTestDemos/TrapWithASMViewController.m
@@ -8,6 +8,19 @@
 
 #import "TrapWithASMViewController.h"
 
+// @see https://github.com/opensource-apple/CF/blob/3cc41a76b1491f50813e28a4ec09954ffa359e6f/CFInternal.h#L145
+#if defined(__i386__) || defined(__x86_64__)
+    #if defined(__GNUC__)
+        #define HALT do {asm __volatile__("int3"); kill(getpid(), 9); __builtin_unreachable(); } while (0)
+    #elif defined(_MSC_VER)
+        #define HALT do { DebugBreak(); abort(); __builtin_unreachable(); } while (0)
+    #else
+        #error Compiler not supported
+    #endif
+#else
+    #define HALT
+#endif
+
 @interface TrapWithASMViewController ()
 
 @end
@@ -16,8 +29,19 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    [self test2];
+}
+
+- (void)test1 {
+#if defined(__i386__) || defined(__x86_64__)
     asm("int3");
     NSLog(@"This line still can be executed after int3");
+#endif
+}
+
+- (void)test2 {
+    HALT;
 }
 
 @end
