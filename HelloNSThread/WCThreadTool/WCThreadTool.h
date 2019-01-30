4aaebbfd634d9afba4446266dd8e8da6d064f3dc
daydreamboy <wesley4chen@gmail.com>
2019-01-30 10:39:43 +0800

    rename HelloThread to HelloNSThread

--- HelloNSThread/WCThreadTool/WCThreadTool.h
+++ HelloNSThread/WCThreadTool/WCThreadTool.h
@@ -8,8 +8,13 @@
 
 #import <Foundation/Foundation.h>
 
+NS_ASSUME_NONNULL_BEGIN
+
 @interface WCThreadTool : NSObject
 
 + (void)performBlock:(dispatch_block_t)block onThread:(NSThread *)thread;
 + (void)performBlock:(void (^)(id))block onThread:(NSThread *)thread withObject:(id)object;
+
 @end
+
+NS_ASSUME_NONNULL_END
