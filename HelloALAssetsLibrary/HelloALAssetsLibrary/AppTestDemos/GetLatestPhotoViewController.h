e76e7288b20d99fcc2e646323827c8ce0e3243d3
daydreamboy <wesley4chen@gmail.com>
2019-02-18 19:36:34 +0800

    add HelloALAssetsLibrary

---
+++ HelloALAssetsLibrary/HelloALAssetsLibrary/AppTestDemos/GetLatestPhotoViewController.h
@@ -0,0 +1,13 @@
+//
+//  GetLatestPhotoViewController.h
+//  HelloALAssetsLibrary
+//
+//  Created by wesley_chen on 2019/2/18.
+//  Copyright © 2019 wesley_chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GetLatestPhotoViewController : UIViewController
+
+@end
