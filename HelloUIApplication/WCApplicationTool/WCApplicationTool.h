64566de4a1a24048c308272cbe645ead20f593c9
daydreamboy <wesley4chen@gmail.com>
2019-03-18 19:22:38 +0800

    HelloUIApplication: add code

--- HelloUIApplication/WCApplicationTool/WCApplicationTool.h
+++ HelloUIApplication/WCApplicationTool/WCApplicationTool.h
@@ -13,10 +13,141 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface WCApplicationTool : NSObject
 
+#pragma mark - App Info
+
+/**
+ App Version (Major Version), viewed in Target's 'General' -> 'Version'
+
+ @return the app version
+ @see http://stackoverflow.com/questions/458632/how-can-my-iphone-app-detect-its-own-version-number
+ */
++ (NSString *)appVersion;
+
+/**
+ App Build Number (Minor Version), viewed in Target's 'General' -> 'Build'
+
+ @return the app build number
+ @see http://stackoverflow.com/questions/458632/how-can-my-iphone-app-detect-its-own-version-number
+ */
++ (NSString *)appBuildNumber;
+
+/**
+ App Product Name
+
+ @return the app display name
+ */
++ (NSString *)appDisplayName;
+
+/**
+ App Binary Executable File Name
+
+ @return the app executable file name
+ */
++ (NSString *)appExecutableName;
+
+/**
+ App Bundle Name
+
+ @return the app bundle name
+ */
++ (NSString *)appBundleName;
+
+/**
+ App Bundle Identifier
+
+ @return the app bundle identifier
+ */
++ (NSString *)appBundleID;
+
+/**
+ App Minimum Supported OS Version
+
+ @return the app minimum supported OS version
+ */
++ (NSString *)appMinimumSupportedOSVersion;
+
+/**
+ The info.plist URL
+
+ @return the info.plist URL
+ */
++ (NSURL *)appInfoPlistURL;
+
+#pragma mark - App Directories
+
+/**
+ Get the path of `Documents` directory
+
+ @return the path of documents directory, e.g. "/.../<app_home>/Documents"
+ */
++ (NSString *)appDocumentsDirectory;
+
+/**
+ Get the path of `Library` directory
+
+ @return the path of library directory, e.g. "/.../<app_home>/Library"
+ */
++ (NSString *)appLibraryDirectory;
+
+/**
+ Get the path of `Caches` directory
+
+ @return the path of Caches directory, e.g. "/.../<app_home>/Library/Caches"
+ */
++ (NSString *)appCachesDirectory;
+
+/**
+ Get the path of 'Home' directory
+
+ @return the path of home directory, e.g. "/.../<app_home>"
+ */
++ (NSString *)appHomeDirectory;
+
+/**
+ Get the path of `tmp` directory
+
+ @return the path of tmp directory, e.g. "/.../tmp/"
+ */
++ (NSString *)appTmpDirectory;
+
+/**
+ Get the path of directory, e.g. Documents, Library, Caches
+
+ @param searchPathDirectory the enum value (NSDocumentDirectory | NSLibraryDirectory | NSCachesDirectory)
+ @return the path of directory
+ */
++ (NSString *)appSearchPathDirectory:(NSSearchPathDirectory)searchPathDirectory;
+
+#pragma mark - App Build Macros
+
++ (BOOL)macroDefined_DEBUG;
++ (BOOL)macroDefined_NDEBUG;
++ (BOOL)macroOn_DEBUG;
++ (BOOL)macroOn_NS_BLOCK_ASSERTIONS;
+
+#pragma mark - App Event
+
+/*!
+ *  enable/disable user interaction
+ *
+ *  @param isAllow YES, enable user interaction; NO, disable user interaction
+ *
+ *  @return If YES, operation is done. If NO, operation is ignored
+ *  @warning
+ *  <br/> 1. This method must be paired with YES and NO
+ *  <br/> 2. This method doesn't work with third-party keyboard on iOS 8+, when disable user interaction but user still can press key
+ */
++ (BOOL)allowUserInteractionEvents:(BOOL)isAllow;
+
+#pragma mark - App Utility
+
+#pragma mark - Risky Methods
+
 /**
  Get the window which owns the presented keyboard
 
  @return the window which presenting the keyboard. Return nil if the keyboard not show
+ @see https://stackoverflow.com/a/45099545
  @discussion Use this method on your risk for it uses private class
  */
 + (nullable UIWindow *)currentKeyboardWindow;
--- HelloUIApplication/WCApplicationTool/WCApplicationTool.m
+++ HelloUIApplication/WCApplicationTool/WCApplicationTool.m
@@ -10,7 +10,152 @@
 
 @implementation WCApplicationTool
 
-// https://stackoverflow.com/a/45099545
+#pragma mark - App Info
+
++ (NSString *)appVersion {
+    // Note: access from memory, maybe changed by some code
+    // [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleShortVersionString"]
+    
+    NSString *version = [[self plistInfo] objectForKey:@"CFBundleShortVersionString"];
+    return version;
+}
+
++ (NSString *)appBuildNumber {
+    // (NSString*)kCFBundleVersionKey is same as \@"CFBundleVersion"
+    NSString *buildNumber = [[self plistInfo] objectForKey:@"CFBundleVersion"];
+    return buildNumber;
+}
+
++ (NSString *)appDisplayName {
+    NSString *displayName = [[self plistInfo] objectForKey:@"CFBundleDisplayName"];
+    return displayName;
+}
+
++ (NSString *)appExecutableName {
+    NSString *executableName = [[self plistInfo] objectForKey:@"CFBundleExecutable"];
+    return executableName;
+}
+
++ (NSString *)appBundleName {
+    // (NSString *)kCFBundleNameKey is same as \@"CFBundleName"
+    NSString *bundleName = [[self plistInfo] objectForKey:@"CFBundleName"];
+    return bundleName;
+}
+
++ (NSString *)appBundleID {
+    NSString *bundleID = [[self plistInfo] objectForKey:@"CFBundleIdentifier"];
+    return bundleID;
+}
+
++ (NSString *)appMinimumSupportedOSVersion {
+    NSString *minimumOSVersion = [[self plistInfo] objectForKey:@"MinimumOSVersion"];
+    return minimumOSVersion;
+}
+
++ (NSURL *)appInfoPlistURL {
+    NSURL *infoPlistURL = [[self plistInfo] objectForKey:@"CFBundleInfoPlistURL"];
+    return infoPlistURL;
+}
+
+#pragma mark ::
+
++ (NSDictionary *)plistInfo {
+    static dispatch_once_t onceToken;
+    static NSDictionary *info;
+    dispatch_once(&onceToken, ^{
+        NSString *filePath = [[NSBundle mainBundle] pathForResource:@"Info" ofType:@"plist"];
+        NSDictionary *dict = [NSDictionary dictionaryWithContentsOfFile:filePath];
+        info = dict ?: [NSBundle mainBundle].infoDictionary;
+    });
+    
+    return info;
+}
+
+#pragma mark ::
+
+#pragma mark - App Directories
+
++ (NSString *)appDocumentsDirectory {
+    return [self appSearchPathDirectory:NSDocumentDirectory];
+}
+
++ (NSString *)appLibraryDirectory {
+    return [self appSearchPathDirectory:NSLibraryDirectory];
+}
+
++ (NSString *)appCachesDirectory {
+    return [self appSearchPathDirectory:NSCachesDirectory];
+}
+
++ (NSString *)appHomeDirectory {
+    return NSHomeDirectory();
+}
+
++ (NSString *)appTmpDirectory {
+    return NSTemporaryDirectory();
+}
+
++ (NSString *)appSearchPathDirectory:(NSSearchPathDirectory)searchPathDirectory {
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(searchPathDirectory, NSUserDomainMask, YES);
+    NSString *directoryPath = ([paths count] > 0) ? paths[0] : nil;
+    return directoryPath;
+}
+
+#pragma mark - App Build Macros
+
++ (BOOL)macroDefined_DEBUG {
+#ifdef DEBUG
+    return YES;
+#else
+    return NO;
+#endif
+}
+
++ (BOOL)macroOn_DEBUG {
+#if DEBUG
+    return YES;
+#else
+    return NO;
+#endif
+}
+
++ (BOOL)macroDefined_NDEBUG {
+#ifdef NDEBUG
+    return YES;
+#else
+    return NO;
+#endif
+}
+
++ (BOOL)macroOn_NS_BLOCK_ASSERTIONS {
+#if NS_BLOCK_ASSERTIONS
+    return YES;
+#else
+    return NO;
+#endif
+}
+
+#pragma mark - App Event
+
++ (BOOL)allowUserInteractionEvents:(BOOL)isAllow {
+    if (isAllow) {
+        if ([[UIApplication sharedApplication] isIgnoringInteractionEvents]) {
+            [[UIApplication sharedApplication] endIgnoringInteractionEvents];
+            return YES;
+        }
+    }
+    else {
+        if (![[UIApplication sharedApplication] isIgnoringInteractionEvents]) {
+            [[UIApplication sharedApplication] beginIgnoringInteractionEvents];
+            return YES;
+        }
+    }
+    
+    return NO;
+}
+
+#pragma mark - App Utility
+
 + (nullable UIWindow *)currentKeyboardWindow {
     NSArray *components = @[ @"UI", @"Remote", @"Keyboard", @"Window" ];
     Class clz = NSClassFromString([components componentsJoinedByString:@""]);
