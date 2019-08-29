5799b190345ffb2aca2facb4405034b95ed4a03c
daydreamboy <wesley4chen@gmail.com>
2019-08-29 14:46:54 +0800

    HelloNSMapTable: add test case

--- HelloNSMapTable/Tests/Tests_NSMapTable.m
+++ HelloNSMapTable/Tests/Tests_NSMapTable.m
@@ -42,4 +42,49 @@
     }
 }
 
+- (void)test_weak_keys {
+    NSMapTable<NSString *, NSString *> *mapTable = [NSMapTable weakToStrongObjectsMapTable];
+    
+    @autoreleasepool {
+        NSMutableArray *keys = [NSMutableArray array];
+        for (NSInteger i = 0; i < 10; i++) {
+            NSString *key = [NSString stringWithFormat:@"%d", (int)i];
+            [keys addObject:key];
+            
+            NSString *value = [NSString stringWithFormat:@"object%d", (int)i];
+            [mapTable setObject:value forKey:key];
+        }
+        
+        // Note: release all the keys
+        [keys removeAllObjects];
+    }
+    
+    for (NSString *key in [mapTable keyEnumerator]) {
+        NSLog(@"%@", key);
+    }
+}
+
+- (void)test_weak_values {
+    NSMapTable<NSString *, NSObject *> *mapTable = [NSMapTable strongToWeakObjectsMapTable];
+    
+    NSMutableArray *values = [NSMutableArray array];
+    for (NSInteger i = 0; i < 10; i++) {
+        NSString *key = [NSString stringWithFormat:@"%d", (int)i];
+        
+        NSObject *value = [NSObject new];
+        [values addObject:value];
+        NSLog(@"%i: %p", (int)i, value);
+        
+//        [mapTable setObject:value forKey:key];
+    }
+    
+    // Note: release all the keys
+    [values removeAllObjects];
+    values = nil;
+    
+    for (NSString *value in [mapTable objectEnumerator]) {
+        NSLog(@"%@", value);
+    }
+}
+
 @end
