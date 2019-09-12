6beacb041b508668bafe643d1c6107a3b3aa0979
daydreamboy <wesley4chen@gmail.com>
2019-09-12 16:58:29 +0800

    HelloXCTestCase: add code

--- HelloXCTestCase/Test/Test.m
+++ HelloXCTestCase/Test/Test.m
@@ -108,4 +108,11 @@ if (!object) { \
     XCTestExpectation_END(40);
 }
 
+- (void)test_measureBlock {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
 @end
