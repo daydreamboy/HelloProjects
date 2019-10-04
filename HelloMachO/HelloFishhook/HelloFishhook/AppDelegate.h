9471df28653f0a45f65fe365a4fd9cb8b99cd32b
daydreamboy <wesley4chen@gmail.com>
2019-10-04 15:48:40 +0800

    HelloMachO: add HelloFishhook

---
+++ HelloMachO/HelloFishhook/HelloFishhook/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloFishhook
+//
+//  Created by wesley_chen on 2019/10/3.
+//  Copyright © 2019 wesley_chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HelloMachO/HelloFishhook/HelloFishhook/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HelloFishhook
+//
+//  Created by wesley_chen on 2019/10/3.
+//  Copyright © 2019 wesley_chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
