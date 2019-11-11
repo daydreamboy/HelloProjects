f2e9a18237c0e966b36a5eeb8bdfe7ff7c449e6f
daydreamboy <wesley4chen@gmail.com>
2019-11-11 20:29:17 +0800

    HelloUIView: add pan gesture

--- HelloUIView/HelloUIView/AppTestDemos/UIView Demo/safeAreaInsets/ShowSafeAreaViewController.m
+++ HelloUIView/HelloUIView/AppTestDemos/UIView Demo/safeAreaInsets/ShowSafeAreaViewController.m
@@ -9,8 +9,13 @@
 #import "ShowSafeAreaViewController.h"
 #import "WCViewTool.h"
 
-@interface ShowSafeAreaViewController ()
+// >= `11.0`
+#ifndef IOS11_OR_LATER
+#define IOS11_OR_LATER          ([[[UIDevice currentDevice] systemVersion] compare:@"11.0" options:NSNumericSearch] != NSOrderedAscending)
+#endif
 
+@interface ShowSafeAreaViewController ()
+@property (nonatomic, assign) CGFloat additionalInsetsValue;
 @end
 
 @implementation ShowSafeAreaViewController
@@ -21,15 +26,20 @@
     CGSize screenSize = [[UIScreen mainScreen] bounds].size;
     NSLog(@"screen size: %@", NSStringFromCGSize(screenSize));
     
+    self.additionalInsetsValue = 0;
     self.view.backgroundColor = [UIColor colorWithRed:157 / 255.0 green:254 / 255.0 blue:157 / 255.0 alpha:1];;
     
     self.childView = [[UILabel alloc] initWithFrame:self.view.bounds];
     self.childView.backgroundColor = [UIColor lightGrayColor];
     self.childView.userInteractionEnabled = YES;
     self.childView.textAlignment = NSTextAlignmentCenter;
+    self.childView.numberOfLines = 0;
     
     UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(childViewTapped:)];
     [self.childView addGestureRecognizer:tapGesture];
+    
+    UIPanGestureRecognizer *panGesture = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(childViewPanned:)];
+    [self.childView addGestureRecognizer:panGesture];
     
     [self.view addSubview:self.childView];
     
@@ -72,6 +82,38 @@
 
 - (void)childViewTapped:(id)sender {
     [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)childViewPanned:(id)sender {
+    CGSize screenSize = [[UIScreen mainScreen] bounds].size;
+    
+    UIPanGestureRecognizer *recognizer = sender;
+    CGPoint translation = [recognizer translationInView:recognizer.view.superview];
+    
+    // Note: make a threhold to limit the maximum of self.additionalInsetsValue
+    CGFloat threshold = screenSize.height / 6.0;
+    
+    // Note: divide by screenSize.height to be a smaller delta value to slow finger moving fast
+    self.additionalInsetsValue += (translation.y / screenSize.height);
+    if (self.additionalInsetsValue < 0) {
+        self.additionalInsetsValue = 0;
+    }
+    else if (self.additionalInsetsValue >= threshold) {
+        self.additionalInsetsValue = threshold;
+    }
+    
+    UIEdgeInsets additionalInsets = UIEdgeInsetsMake(self.additionalInsetsValue, self.additionalInsetsValue, self.additionalInsetsValue, self.additionalInsetsValue);
+    
+    if (IOS11_OR_LATER) {
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunguarded-availability-new"
+        self.additionalSafeAreaInsets = additionalInsets;
+#pragma GCC diagnostic pop
+    }
+    
+    // Note: to trigger viewWillLayoutSubviews and viewDidLayoutSubviews
+    [self.view setNeedsLayout];
+    [self.view layoutIfNeeded];
 }
 
 @end
