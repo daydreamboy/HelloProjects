a068cd9437374b09cc685d15fd782de1ffba2eb7
daydreamboy <wesley4chen@gmail.com>
2020-05-15 21:58:40 +0800

    HelloNSThread: add code

--- HelloNSThread/HelloNSThread/AppTestDemos/callStack/GetSymbolicCallStackViewController.m
+++ HelloNSThread/HelloNSThread/AppTestDemos/callStack/GetSymbolicCallStackViewController.m
@@ -8,49 +8,48 @@
 
 #import "GetSymbolicCallStackViewController.h"
 #import <mach-o/dyld.h>
+#import "WCThreadTool.h"
 
 @interface GetSymbolicCallStackViewController ()
-- (void)methodNotExists;
+@property (nonatomic, strong) UITextView *textView;
 @end
 
 @implementation GetSymbolicCallStackViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.view.backgroundColor = [UIColor whiteColor];
     
-    [self makeACrash];
+    UIBarButtonItem *viewCallStackItem = [[UIBarButtonItem alloc] initWithTitle:@"View CallStack" style:UIBarButtonItemStylePlain target:self action:@selector(viewCallStackItem:)];
+    
+    self.navigationItem.rightBarButtonItems = @[viewCallStackItem];
+    
+    [self.view addSubview:self.textView];
 }
 
-- (void)makeACrash {
-//    @try {
-        [self methodNotExists];
-//    } @catch (NSException *exception) {
-//        NSArray<NSString *> *callStackSymbols = [exception callStackSymbols];
-//
-//        NSString *loadImageAddress = [self getPrismImageLoadAddress];
-//        NSString *backtrace = [callStackSymbols componentsJoinedByString:@"\n"];
-//        NSString *log = [[NSString alloc] initWithFormat:@"\nprism Load image address:%@\ncall backtrace:\n%@", loadImageAddress, backtrace];
-//        
-//        NSLog(@"%@", log);
-//    } @finally {
-//
-//    }
+- (void)viewDidLayoutSubviews {
+    [super viewDidLayoutSubviews];
+    
+    self.textView.frame = self.view.bounds;
 }
 
-- (NSString *)getPrismImageLoadAddress {
-    const struct mach_header *executableHeader = NULL;
-    //遍历所有绑定运行的image
-    for (uint32_t i = 0; i < _dyld_image_count(); i++){
-        const struct mach_header *header = _dyld_get_image_header(i);
-       //找到可执行文件类型的image
-        if (header->filetype == MH_EXECUTE) {
-            executableHeader = header;
-            break;
-        }
+#pragma mark - Getter
+
+- (UITextView *)textView {
+    if (!_textView) {
+        CGSize screenSize = [[UIScreen mainScreen] bounds].size;
+        UITextView *textView = [[UITextView alloc] initWithFrame:CGRectMake(0, 64, screenSize.width, screenSize.height)];
+        
+        _textView = textView;
     }
-    //将指针地址转换成string类型
-    NSString *address = [NSString stringWithFormat:@"0x%lx", (NSInteger)executableHeader];
-    return address;
+    
+    return _textView;
+}
+
+#pragma mark - Action
+
+- (void)viewCallStackItem:(id)sender {
+    self.textView.text = [WCThreadTool currentThreadCallStackString];
 }
 
 @end
