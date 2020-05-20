115f70a4feffcce19a7f69c8bd0458efb9042821
daydreamboy <wesley4chen@gmail.com>
2020-05-20 11:57:55 +0800

    HelloXcodeproj: delete files

--- HelloXcodeproj/03 - Ruby Tasks/40_string_gsub.rb
+++
@@ -1,9 +0,0 @@
-
-arr = %w(
-    "WXSDK/project/tcmpushsdk/inet/inet/tcm/tcmprotocol/TcmslogsrvPack.{h,m,mm,c,cpp}",
-    "WXSDK/project/tcmpushsdk/inet/inet/tcm/tcmprotocol/WxaccessPack.{h,m,mm,c,cpp}",
-)
-
-replaced_arr = arr.map { |item| item.gsub(/\..*/, ".h") }
-
-puts replaced_arr
