77e07d10cb7203163be64dbc8df0d85f1d983b57
daydreamboy <wesley4chen@gmail.com>
2020-08-01 23:04:20 +0800

    HelloCoreAnimation: adjust files

--- HelloCoreAnimation/HelloCoreAnimation/AppTestDemos/CALayer Presentation/CAShapeLayer/UseCAGradientLayerWithTwoColorsViewController.m
+++ HelloCoreAnimation/HelloCoreAnimation/AppTestDemos/CALayer Presentation/CAShapeLayer/UseCAGradientLayerWithTwoColorsViewController.m
@@ -10,10 +10,6 @@
 #import "WCViewTool.h"
 #import "WCMacroTool.h"
 
-#ifndef UICOLOR_RGB
-#define UICOLOR_RGB(color)       [UIColor colorWithRed: (((color) >> 16) & 0xFF) / 255.0 green: (((color) >> 8) & 0xFF) / 255.0 blue: ((color) & 0xFF) / 255.0 alpha: 1.0]
-#endif
-
 @interface UseCAGradientLayerWithTwoColorsViewController ()
 @property (nonatomic, strong) UIView *view1;
 @property (nonatomic, strong) UIView *view2;
