466c9616b8f44676932795a5e26298a5bc74ef48
daydreamboy <wesley4chen@gmail.com>
2020-08-09 20:10:52 +0800

    HelloUIScrollView: add code

---
+++ HelloUIScrollView/HelloUIScrollView/AppTestDemos/StickyPushSectionInScrollViewViewController.m
@@ -0,0 +1,32 @@
+//
+//  StickyPushSectionInScrollViewViewController.m
+//  HelloUIScrollView
+//
+//  Created by wesley_chen on 2020/8/9.
+//  Copyright © 2020 wesley_chen. All rights reserved.
+//
+
+#import "StickyPushSectionInScrollViewViewController.h"
+
+@interface StickyPushSectionInScrollViewViewController ()
+
+@end
+
+@implementation StickyPushSectionInScrollViewViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
