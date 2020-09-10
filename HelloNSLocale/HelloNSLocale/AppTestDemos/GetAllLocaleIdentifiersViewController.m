5d74201bd044b71e0dface1daa5da73cd97179db
daydreamboy <wesley4chen@gmail.com>
2020-09-10 14:51:16 +0800

    add HelloNSLocale

---
+++ HelloNSLocale/HelloNSLocale/AppTestDemos/GetAllLocaleIdentifiersViewController.m
@@ -0,0 +1,67 @@
+//
+//  GetAllLocaleIdentifiersViewController.m
+//  HelloNSLocale
+//
+//  Created by wesley_chen on 2020/9/10.
+//  Copyright © 2020 wesley_chen. All rights reserved.
+//
+
+#import "GetAllLocaleIdentifiersViewController.h"
+
+@interface GetAllLocaleIdentifiersViewController () <UITableViewDelegate, UITableViewDataSource>
+@property (nonatomic, strong) UITableView *tableView;
+@property (nonatomic, strong) NSArray<NSString *> *listData;
+@end
+
+@implementation GetAllLocaleIdentifiersViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self setup];
+    
+    self.view.backgroundColor = [UIColor whiteColor];
+    [self.view addSubview:self.tableView];
+}
+
+- (void)setup {
+    self.listData = [[NSLocale availableLocaleIdentifiers] sortedArrayUsingSelector:@selector(compare:)];
+}
+
+#pragma mark - Getters
+
+- (UITableView *)tableView {
+    if (!_tableView) {
+        CGSize screenSize = [[UIScreen mainScreen] bounds].size;
+        UITableView *tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, screenSize.width, screenSize.height - 64) style:UITableViewStylePlain];
+        tableView.delegate = self;
+        tableView.dataSource = self;
+        
+        _tableView = tableView;
+    }
+    
+    return _tableView;
+}
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [self.listData count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *sCellIdentifier = @"FadeInCellRowByRowViewController_sCellIdentifier";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:sCellIdentifier];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:sCellIdentifier];
+    }
+    
+    cell.textLabel.text = self.listData[indexPath.row];
+    
+    return cell;
+}
+
+@end
