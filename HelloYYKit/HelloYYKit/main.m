272553d88f9168dd7b88fd83c22b54eff8be9d6d
daydreamboy <wesley4chen@gmail.com>
2020-11-12 16:10:41 +0800

    add HelloYYKit

---
+++ HelloYYKit/HelloYYKit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HelloYYKit
+//
+//  Created by wesley_chen on 2020/11/12.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    NSString * appDelegateClassName;
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+        appDelegateClassName = NSStringFromClass([AppDelegate class]);
+    }
+    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}
