3e75201e31082ebd1e6917b21cb9c74609166d71
daydreamboy <wesley4chen@gmail.com>
2021-03-09 00:03:15 +0800

    HelloLottie: add code

--- HelloLottie/HelloLottie/AppTestDemos/Demo1ViewController.m
+++ HelloLottie/HelloLottie/AppTestDemos/Demo1ViewController.m
@@ -7,21 +7,30 @@
 //
 
 #import "Demo1ViewController.h"
+#import <Lottie/Lottie.h>
 
 @interface Demo1ViewController ()
-
 @end
 
 @implementation Demo1ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    NSString *pathComponent = @"rotateRefresh/data.json";
+    pathComponent = @"rotateRefresh-focus/data.json";
+    
+    NSString *path = [[[NSBundle mainBundle] bundlePath] stringByAppendingPathComponent:pathComponent];
+    LOTComposition *composition = [LOTComposition animationWithFilePath:path];
+    LOTAnimationView *animationView = [[LOTAnimationView alloc] initWithModel:composition inBundle:[NSBundle mainBundle]];
+    animationView.frame = CGRectMake(0, 0, 200, 200);
+    animationView.hidden = NO;
+    
+    [self.view addSubview:animationView];
+    [animationView playToProgress:1 withCompletion:^(BOOL animationFinished) {
+        
+    }];
 }
 
 @end
